1bdeb010bf84d808a94f77c1fccf38899be8e80c
00krp <89786258+00krp@users.noreply.github.com>
2023-10-09 12:43:47 +1100

    --

--- php/config.php
+++ php/config.php
@@ -1,12 +1,11 @@
 <?php
     $DB_HOST="aws.connect.psdb.cloud";
-    $DB_USERNAME="j8lj4rxa0u1xgi2k2u7k";
-    $DB_PASSWORD="pscale_pw_AWltbBtBzBRbTxoNRsr4Y8mC6aQ49dTtIvThpGABvnA";
+    $DB_USERNAME="22htlxtybsgmxhicztkl";
+    $DB_PASSWORD="pscale_pw_MeNGYcXnh0zq5AKeF7jwTThT6VvGaHWWQNF54OPAKOs";
     $DB_NAME="gotogrodb";
     $MYSQL_ATTR_SSL_CA="/etc/ssl/cert.pem";
 
     $conn = mysqli_init();
     $conn->ssl_set(NULL, NULL, $MYSQL_ATTR_SSL_CA, NULL, NULL);
     $conn->real_connect($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
-    
 ?>
