0e699492c7076a696d65f0de3c32aaa1cec5fe8d
CodyDinh0502 <145318118+CodyDinh0502@users.noreply.github.com>
2023-10-21 00:15:36 +1100

    Update SalesReport.php

--- SalesReport.php
+++ SalesReport.php
@@ -11,69 +11,65 @@
 </head>
 
 <body>
-	<h1>Member Managment System</h1>
-    <!--Company Logo-->
-    <img src="css\images\Goto_Logo.png" alt="Goto Logo"  width="50" height="50" style="float:Left;">
-
-	<!--Hoverable Menu (Add CSS to make it dropdown)-->
-	<div class="Menu">
-		<button class="Menu">Menu</button>
-		<div class="Menu">
-			<!--Add the html links-->
-		<a href="Main.php">Home</a>
-        <a href="MemberList.php">Members</a>
-        <a href="ProductList.php">Products</a>
-        <a href="SalesReport.php">Sales Report</a>
-
+	<div class='nav'>
+		<img id='logo' src="css\images\Goto_Logo.png" alt="Goto Logo"  width="50" height="50" style="float:Left;">
+		<h1 id='title'>GotoGro MRM</h1>
+	
+		<div class="menu">
+			<a href="Main.php">Home</a>
+			<a href="MemberList.php">Members</a>
+			<a href="ProductList.php">Products</a>
+			<a href="SalesReport.php">Sales Report</a>
 		</div>
 	</div>
 
-	<h2 id=pgHeader>Sales</h2>
-	<div class="tbldata">
-		<p><table>
-			<thead><tr>
-				<th>Sale ID</th>
-				<th>Member ID</th>
-				<th>Product ID</th>
-				<th>Quantity</th>
-				<th>Date</th>
-				<th>Price</th>
-			</tr></thead>
+	<div class='content'>
+		<h2 id=pgHeader>Sales</h2>
+		<div class="tbldata">
+			<p><table>
+				<thead><tr>
+					<th>Sale ID</th>
+					<th>Member ID</th>
+					<th>Product ID</th>
+					<th>Quantity</th>
+					<th>Date</th>
+					<th>Price</th>
+				</tr></thead>
+	
+				<!-- display data -->
+				<tbody id="phpTable">
+					<?php 
+						include ("php/config.php");
+					
+						//all data from member table
+						$query = "SELECT * FROM sale";
+					
+						//retrieve data from table
+						$result = mysqli_query($conn, $query);
+					
+						while ($row = mysqli_fetch_assoc($result)) {
+							echo "<tr>";
+							echo "<td>" . $row['saleID'] . "</td>";
+							echo "<td>" . $row['memID'] . "</td>";
+							echo "<td>" . $row['pdID'] . "</td>";
+							echo "<td>" . $row['quantity'] . "</td>";
+							echo "<td>" . $row['saleDate'] . "</td>";
+							echo "<td>" . $row['salePrice'] . "</td>";
+							echo "<td><button><a href=\"SaleEditForm.php?updateID=" . $row['saleID'] . "\">Update</a></button>";
+							echo "<button><a href=\"SaleDeleteForm.php?deleteID=" . $row['saleID'] . "\">Delete</a></button></td>";
+							echo "</tr>";
+						}
+					?>
+				</tbody> 
+				
+			</table></p>
+		</div>
 
-			<!-- display data -->
-			<tbody id="phpTable">
-				<?php 
-					include ("php/config.php");
-				
-					//all data from member table
-					$query = "SELECT * FROM sale";
-				
-					//retrieve data from table
-					$result = mysqli_query($conn, $query);
-				
-					while ($row = mysqli_fetch_assoc($result)) {
-						echo "<tr>";
-						echo "<td>" . $row['saleID'] . "</td>";
-						echo "<td>" . $row['memID'] . "</td>";
-						echo "<td>" . $row['pdID'] . "</td>";
-						echo "<td>" . $row['quantity'] . "</td>";
-						echo "<td>" . $row['saleDate'] . "</td>";
-						echo "<td>" . $row['salePrice'] . "</td>";
-						echo "<td><button><a href=\"SaleEditForm.php?updateID=" . $row['saleID'] . "\">Update</a></button>";
-						echo "<button><a href=\"SaleDeleteForm.php?deleteID=" . $row['saleID'] . "\">Delete</a></button></td>";
-						echo "</tr>";
-					}
-				?>
-			</tbody> 
-			
-		</table></p>
+		<button><a href="SalesAddForm.php">Manually Add Sales</a></button>
+		<form method="post" action="php/SaleReportExport.php" class="dbForm">
+			<input type='submit' value='Export to CSV' name='Export to CSV'/>
+		</form>
 	</div>
-
-	<button><a href="SalesAddForm.php">Manually Add Sales</a></button>
-	<form method="post" action="php/SaleReportExport.php" class="dbForm">
-		<input type='submit' value='Export to CSV' name='Export to CSV'/>
-	</form>
-
 </body>
 
 </html>
