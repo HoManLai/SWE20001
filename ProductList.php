76625f0013fdcac95f2d3ca925e3518e9ca1856a
CodyDinh0502 <145318118+CodyDinh0502@users.noreply.github.com>
2023-10-21 00:12:43 +1100

    Update ProductList.php

--- ProductList.php
+++ ProductList.php
@@ -11,67 +11,65 @@
 </head>
 
 <body>
-	<h1>Member Managment System</h1>
-    <!--Company Logo-->
-    <img src="css\images\Goto_Logo.png" alt="Goto Logo"  width="50" height="50" style="float:Left;">
-
-	<!--Hoverable Menu (Add CSS to make it dropdown)-->
-	<div class="Menu">
-		<button class="Menu">Menu</button>
-		<div class="Menu">
-			<!--Add the html links-->
-		<a href="Main.php">Home</a>
-        <a href="MemberList.php">Members</a>
-        <a href="ProductList.php">Products</a>
-        <a href="SalesReport.php">Sales Report</a>
-
+	<div class='nav'>
+		<img id='logo' src="css\images\Goto_Logo.png" alt="Goto Logo"  width="50" height="50" style="float:Left;">
+		<h1 id='title'>GotoGro MRM</h1>
+	
+		<div class="menu">
+			<a href="Main.php">Home</a>
+			<a href="MemberList.php">Members</a>
+			<a href="ProductList.php">Products</a>
+			<a href="SalesReport.php">Sales Report</a>
 		</div>
 	</div>
 
-	<h2 id=pgHeader>Products</h2>
-    <div class="tbldata">
-        <p><table>
-            <thead><tr>
-                <th>Product ID</th>
-                <th>Name</th>
-                <th>Category</th>
-                <th>Price</th>
-                <th>Supplier</th>
-                <th>Description</th>
-            </tr></thead>
+	<div class='content'>
+		<h2 id=pgHeader>Products</h2>
+	    	<div class="tbldata">
+	        <p><table>
+	            <thead><tr>
+	                <th>Product ID</th>
+	                <th>Name</th>
+	                <th>Category</th>
+	                <th>Price</th>
+	                <th>Supplier</th>
+	                <th>Description</th>
+	            </tr></thead>
+	
+				<tbody id="phpTable">
+					<?php 
+						include ("php/config.php");
+					
+						//all data from member table
+						$query = "SELECT * FROM product";
+					
+						//retrieve data from table
+						$result = mysqli_query($conn, $query);
+	
+						while ($row = mysqli_fetch_assoc($result)) {
+							echo "<tr>";
+							echo "<td>" . $row['pdID'] . "</td>";
+							echo "<td>" . $row['pdName'] . "</td>";
+							echo "<td>" . $row['category'] . "</td>";
+							echo "<td>" . $row['price'] . "</td>";
+							echo "<td>" . $row['supplier'] . "</td>";
+							echo "<td>" . $row['description'] . "</td>";
+							echo "<td><button><a href=\"ProductEditForm.php?updateID=" . $row['pdID'] . "\">Update</a></button>";
+							echo "<button><a href=\"ProductDeleteForm.php?deleteID=" . $row['pdID'] . "\">Delete</a></button></td>";
+							echo "</tr>";
+						}
+					?>
+				</tbody> 
+	            
+	        </table></p>
+	</div>
 
-			<tbody id="phpTable">
-				<?php 
-					include ("php/config.php");
-				
-					//all data from member table
-					$query = "SELECT * FROM product";
-				
-					//retrieve data from table
-					$result = mysqli_query($conn, $query);
+		<a href="ProductAddForm.php">Add Products</a>
+		<form method="post" action="php/ProductListExport.php" class="dbForm">
+			<input type='submit' value='Export to CSV' name='Export to CSV'/>
+		</form>
+	</div>
 
-					while ($row = mysqli_fetch_assoc($result)) {
-						echo "<tr>";
-						echo "<td>" . $row['pdID'] . "</td>";
-						echo "<td>" . $row['pdName'] . "</td>";
-						echo "<td>" . $row['category'] . "</td>";
-						echo "<td>" . $row['price'] . "</td>";
-						echo "<td>" . $row['supplier'] . "</td>";
-						echo "<td>" . $row['description'] . "</td>";
-						echo "<td><button><a href=\"ProductEditForm.php?updateID=" . $row['pdID'] . "\">Update</a></button>";
-						echo "<button><a href=\"ProductDeleteForm.php?deleteID=" . $row['pdID'] . "\">Delete</a></button></td>";
-						echo "</tr>";
-					}
-				?>
-			</tbody> 
-            
-        </table></p>
-    </div>
-
-	<a href="ProductAddForm.php">Add Products</a>
-	<form method="post" action="php/ProductListExport.php" class="dbForm">
-		<input type='submit' value='Export to CSV' name='Export to CSV'/>
-	</form>
 </body>
 
 </html>
