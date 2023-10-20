9309e112127d5b98333262161f64594dd9ba3afc
CodyDinh0502 <145318118+CodyDinh0502@users.noreply.github.com>
2023-10-20 23:59:53 +1100

    Update Main.php
    update HTML for nav bar

--- Main.php
+++ Main.php
@@ -10,28 +10,22 @@
 </head>
 
 <body>
-	<h1>Member Managment System</h1>
-    <!--Company Logo-->
-    <img src="css\images\Goto_Logo.png" alt="Goto Logo"  width="50" height="50" style="float:Left;">
-
-	<!--Hoverable Menu (Add CSS to make it dropdown)-->
-	<div class="Menu">
-		<button class="Menu">Menu</button>
-		<div class="Menu">
-			<!--Add the html links-->
-		<a href="Main.php">Home</a>
-        <a href="MemberList.php">Members</a>
-        <a href="ProductList.php">Products</a>
-        <a href="SalesReport.php">Sales Report</a>
-
+	<div class='nav'>
+		<img id='logo' src="css\images\Goto_Logo.png" alt="Goto Logo"  width="50" height="50" style="float:Left;">
+		<h1 id='title'>GotoGro MRM</h1>
+	
+		<div class="menu">
+			<a href="Main.php">Home</a>
+			<a href="MemberList.php">Members</a>
+			<a href="ProductList.php">Products</a>
+			<a href="SalesReport.php">Sales Report</a>
 		</div>
 	</div>
-
-	<!--Display and analysis members dtails and sales records by charts-->
-	<!--Sprint 2-->
-	<h2>Recent Sales</h2>
-	<h2>Recent Orders</h2>
-	<h2>New Members</h2>
+	<div class='content'>
+		<h3>Recent Sales</h3>
+		<h3>Recent Orders</h3>
+		<h3>New Members</h3>
+	</div>
 </body>
 
 </html>
