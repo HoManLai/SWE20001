2e4c2eac260daea4298601378070fe5038c204a0
CodyDinh0502 <145318118+CodyDinh0502@users.noreply.github.com>
2023-10-21 00:05:58 +1100

    Update MemberEditForm.php

--- MemberEditForm.php
+++ MemberEditForm.php
@@ -11,20 +11,15 @@
 </head>
 
 <body>
-	<h1>Member Managment System</h1>
-    <!--Company Logo-->
-    <img src="css\images\Goto_Logo.png" alt="Goto Logo"  width="50" height="50" style="float:Left;">
-
-	<!--Hoverable Menu (Add CSS to make it dropdown)-->
-	<div class="Menu">
-		<button class="Menu">Menu</button>
-		<div class="Menu">
-			<!--Add the html links-->
-		<a href="Main.php">Home</a>
-        <a href="MemberList.php">Members</a>
-        <a href="ProductList.php">Products</a>
-        <a href="SalesReport.php">Sales Report</a>
-
+	<div class='nav'>
+		<img id='logo' src="css\images\Goto_Logo.png" alt="Goto Logo"  width="50" height="50" style="float:Left;">
+		<h1 id='title'>GotoGro MRM</h1>
+	
+		<div class="menu">
+			<a href="Main.php">Home</a>
+			<a href="MemberList.php">Members</a>
+			<a href="ProductList.php">Products</a>
+			<a href="SalesReport.php">Sales Report</a>
 		</div>
 	</div>
 
@@ -49,45 +44,46 @@
 	$postcode = $row['postcode'];
 	$dob = $row['dob'];
 ?>
-
-	<div class="container">
-		<form method="post"  class="dbForm">
-			<fieldset>
-				<legend>Edit Member Details</legend>
-				<label for "memID">ID: </label>
-					<input type="text" name="memID" id="memID" value="<?php echo $memID ?>" readonly/>
-				<label for "memFirst">First Name: </label>
-					<input type="text" name="memFirst" id="memFirst" value="<?php echo $memFirst ?>"/>
-				<label for "memLast">Last Name: </label>
-					<input type="text" name="memLast" id="memLast" value="<?php echo $memLast ?>"/>
-				<label for "phone">Phone Number: </label>
-					<input type="text" name="phone" id="phone" value="<?php echo $phone ?>"/>
-				<label for "email">Email: </label>
-					<input type="text" name="email" id="email" value="<?php echo $email ?>"/>
-				<label for "streetName">Address: </label>
-					<input type="text" name="streetName" id="streetName" value="<?php echo $streetName ?>"/>
-				<label for "suburb">Suburb: </label>
-					<input type="text" name="suburb" id="suburb" value="<?php echo $suburb ?>"/>
-				<label for="state">State:</label>
-					<select id="state" name="state">
-					<option value="<?php echo $state ?>"><?php echo $state ?></option>
-					<option value="ACT">ACT</option>
-					<option value="NSW">NSW</option>
-					<option value="NT">NT</option>
-					<option value="SA">SA</option>
-					<option value="TAS">TAS</option>
-					<option value="VIC">VIC</option>
-					<option value="WA">WA</option>
-					</select>
-				<label for "postcode">Postcode: </label>
-					<input type="text" name="postcode" id="postcode" value="<?php echo $postcode ?>"/>
-				<label for "dob">Date of Birth: </label>
-					<input type="date" name="dob" id="dob" value="<?php echo date("Y-m-d", strtotime($dob)) ?>" />
-				
-				<input id="submit" type="submit" value="Edit Member"/>
-				<button><a href='MemberList.php'>Go Back</a></button>
-			</fieldset>
-		</form>
+	<div class='content'>
+		<div class="container">
+			<form method="post"  class="dbForm">
+				<fieldset>
+					<legend>Edit Member Details</legend>
+					<label for "memID">ID: </label>
+						<input type="text" name="memID" id="memID" value="<?php echo $memID ?>" readonly/>
+					<label for "memFirst">First Name: </label>
+						<input type="text" name="memFirst" id="memFirst" value="<?php echo $memFirst ?>"/>
+					<label for "memLast">Last Name: </label>
+						<input type="text" name="memLast" id="memLast" value="<?php echo $memLast ?>"/>
+					<label for "phone">Phone Number: </label>
+						<input type="text" name="phone" id="phone" value="<?php echo $phone ?>"/>
+					<label for "email">Email: </label>
+						<input type="text" name="email" id="email" value="<?php echo $email ?>"/>
+					<label for "streetName">Address: </label>
+						<input type="text" name="streetName" id="streetName" value="<?php echo $streetName ?>"/>
+					<label for "suburb">Suburb: </label>
+						<input type="text" name="suburb" id="suburb" value="<?php echo $suburb ?>"/>
+					<label for="state">State:</label>
+						<select id="state" name="state">
+						<option value="<?php echo $state ?>"><?php echo $state ?></option>
+						<option value="ACT">ACT</option>
+						<option value="NSW">NSW</option>
+						<option value="NT">NT</option>
+						<option value="SA">SA</option>
+						<option value="TAS">TAS</option>
+						<option value="VIC">VIC</option>
+						<option value="WA">WA</option>
+						</select>
+					<label for "postcode">Postcode: </label>
+						<input type="text" name="postcode" id="postcode" value="<?php echo $postcode ?>"/>
+					<label for "dob">Date of Birth: </label>
+						<input type="date" name="dob" id="dob" value="<?php echo date("Y-m-d", strtotime($dob)) ?>" />
+					
+					<input id="submit" type="submit" value="Edit Member"/>
+					<button><a href='MemberList.php'>Go Back</a></button>
+				</fieldset>
+			</form>
+		</div>
 	</div>
 
 </body>
