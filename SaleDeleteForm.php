4fb58431dae454866ed7da7d49d8803b82c84a0f
CodyDinh0502 <145318118+CodyDinh0502@users.noreply.github.com>
2023-10-21 00:13:29 +1100

    Update SaleDeleteForm.php

--- SaleDeleteForm.php
+++ SaleDeleteForm.php
@@ -11,67 +11,64 @@
 </head>
 
 <body>
-	<h1>Member Managment System</h1>
-    <!--Company Logo-->
-    <img src="css\images\Goto_Logo.png" alt="Goto Logo"  width="50" height="50" style="float:Left;">
-
-	<!--Hoverable Menu (Add CSS to make it dropdown)-->
-	<div class="Menu">
-		<button class="Menu">Menu</button>
-		<div class="Menu">
-			<!--Add the html links-->
-		<a href="Main.php">Home</a>
-        <a href="MemberList.php">Members</a>
-        <a href="ProductList.php">Products</a>
-        <a href="SalesReport.php">Sales Report</a>
-
+	<div class='nav'>
+		<img id='logo' src="css\images\Goto_Logo.png" alt="Goto Logo"  width="50" height="50" style="float:Left;">
+		<h1 id='title'>GotoGro MRM</h1>
+	
+		<div class="menu">
+			<a href="Main.php">Home</a>
+			<a href="MemberList.php">Members</a>
+			<a href="ProductList.php">Products</a>
+			<a href="SalesReport.php">Sales Report</a>
 		</div>
 	</div>
 
-	<div class="tbldata">
-        <p><table>
-			<thead><tr>
-				<th>Sale ID</th>
-				<th>Member ID</th>
-				<th>Product ID</th>
-				<th>Quantity</th>
-				<th>Date</th>
-				<th>Price</th>
-			</tr></thead>
-
-			<tbody id="phpTable">
-				<?php 
-					include ("php/config.php");
-			
-					$saleID=$_GET["deleteID"];
-
-					$query = "SELECT * FROM sale WHERE saleID=" . $saleID;
-
-					//retrieve data from table
-					$result = mysqli_query($conn, $query);
-
-					//store in array
-					$row = mysqli_fetch_assoc($result);
-					echo "<tr>";
-					echo "<td>" . $row['saleID'] . "</td>";
-					echo "<td>" . $row['memID'] . "</td>";
-					echo "<td>" . $row['pdID'] . "</td>";
-					echo "<td>" . $row['quantity'] . "</td>";
-					echo "<td>" . $row['saleDate'] . "</td>";
-					echo "<td>" . $row['salePrice'] . "</td>";
-					echo "</tr>";
-				?>
-			</tbody>
-		</table></p>
+	<div class='content'>
+		<div class="tbldata">
+	        <p><table>
+				<thead><tr>
+					<th>Sale ID</th>
+					<th>Member ID</th>
+					<th>Product ID</th>
+					<th>Quantity</th>
+					<th>Date</th>
+					<th>Price</th>
+				</tr></thead>
+	
+				<tbody id="phpTable">
+					<?php 
+						include ("php/config.php");
+				
+						$saleID=$_GET["deleteID"];
+	
+						$query = "SELECT * FROM sale WHERE saleID=" . $saleID;
+	
+						//retrieve data from table
+						$result = mysqli_query($conn, $query);
+	
+						//store in array
+						$row = mysqli_fetch_assoc($result);
+						echo "<tr>";
+						echo "<td>" . $row['saleID'] . "</td>";
+						echo "<td>" . $row['memID'] . "</td>";
+						echo "<td>" . $row['pdID'] . "</td>";
+						echo "<td>" . $row['quantity'] . "</td>";
+						echo "<td>" . $row['saleDate'] . "</td>";
+						echo "<td>" . $row['salePrice'] . "</td>";
+						echo "</tr>";
+					?>
+				</tbody>
+			</table></p>
+		</div>
+	
+		<div class="container">
+			<form method="post"  class="dbForm">
+				<p>Do you really want to delete this sale?</p>
+				<button><a href='SalesReport.php'>Go Back</a></button>
+				<input id="submit" type="submit" name="submit" value="Confirm"/>
+			</form>
+		</div>
 	</div>
-
-	<div class="container">
-		<form method="post"  class="dbForm">
-			<p>Do you really want to delete this sale?</p>
-			<button><a href='SalesReport.php'>Go Back</a></button>
-			<input id="submit" type="submit" name="submit" value="Confirm"/>
-		</form>
-
 </body>
 </html>
 
